ac0928061899f2cf2f9eb77d9b26bf485949a08d
kacper <kacper_0707@o2.pl>
2023-07-23 10:05:36 +0200

    Updating tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  content: {
+    relative: true,
+    files: ['./build/*.html']
+  },
   theme: {
-    extend: {},
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+    },
+    extend: {}
   },
   plugins: [],
 }
